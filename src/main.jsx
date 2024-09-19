4a9f287dcf8e6f85f4c071f720676b81f9180cb2
Anurag kumar <anuragk.c11@gmail.com>
2024-09-19 21:28:32 +0530

    committed

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import StoreContextProvider from './context/storeContext.jsx'
+import {HashRouter as Router} from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <Router>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+  </Router>
 )
