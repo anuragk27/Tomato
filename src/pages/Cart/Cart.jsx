0705048cc82896bf744e1b6f3c6dd60ce48a07c7
Anurag kumar <anuragk.c11@gmail.com>
2024-09-20 09:43:01 +0530

    dollar to rupee

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -46,17 +46,17 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>₹{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
                 <p>Delivery Charge</p>
-                <p>${getTotalCartAmount()==0?0:2}</p>
+                <p>₹{getTotalCartAmount()==0?0:20}</p>
             </div>
             <hr />
             <div className="cart-total-details">
                 <b>Total</b>
-                <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+                <b>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+20}</b>
             </div>
           </div>
           <button onClick={() => navigate('/order')} >PROCEED TO CHECKOUT</button>
